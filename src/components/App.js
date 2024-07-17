0afd70cc44ee3a0e07e857408587ec741fd25001
Alkeshkumar94 <alkeshkumar949@gmail.com>
2024-07-17 09:25:33 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@
 import React from "react";
 import './../styles/App.css';
 
+import Greeting from './greeting'; // Adjust the path as necessary
+
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Greeting />
     </div>
-  )
-}
-
+  );
+};
 export default App
